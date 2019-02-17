8f16a1ae8643dd9fef5d502e7a91c111eea32ab4
Tracy Ngo <tngo1206@gmail.com>
2019-02-17 05:40:21 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -187,11 +187,11 @@ describe "#display_board in 'lib/display_board.rb" do
       output = capture_puts{ display_board(board) }
       rows = output.split("\n")
 
-      expect(rows[0]).to eq(" X | O | X ")
+      expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" O | X | X ")
+      expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" O | X | O ")
+      expect(rows[4]).to eq(" O | O | O ")
 
       # *** Comment the line below by adding a # at the line start ***
      # expect(true).to be(true)
