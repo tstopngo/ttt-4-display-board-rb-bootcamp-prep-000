0d4cf0a18653fb3e2eae2eb9867edfe3831424a9
Tracy Ngo <tngo1206@gmail.com>
2019-02-17 05:31:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,10 +4,10 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-  puts " #{board[0]}  |   |   "
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
 end
   
